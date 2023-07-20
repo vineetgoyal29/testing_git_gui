c0ef7cce8537c7244e8594d83be08909ebeb84ab
vineetgoyal29 <vineet.goyel@gmail.com>
2023-07-20 08:51:56 +0530

    intial commit

---
+++ Fib.java
@@ -0,0 +1,21 @@
+
+public class Fib {
+	
+		public static void main(String args[])
+		{  
+		 int n1=Integer.parseInt(args[0]);
+		 int n2=n1;
+		 int n3,i,count=10;  
+		 System.out.print(n1+" "+n2); 
+		  
+		 for(i=2;i<count;++i)
+		 {  
+		  n3=n1+n2;  
+		  System.out.print(" "+n3);  
+		  n1=n2;  
+		  n2=n3;  
+		 }  
+
+		}}
+
+
